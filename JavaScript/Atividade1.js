3b780cde7b87ccf0055114862d3d59dd67ea01cc
Euder Alves <euder.alv@gmail.com>
2021-06-19 20:43:19 -0300

    Atividade 1-3

---
+++ JavaScript/Atividade1.js
@@ -0,0 +1,40 @@
+/* 
+//Atividade1
+let a = 10;
+if (a > 15)
+console.log(1, 1);
+else
+if(a > 3)
+if(a > 15)
+console.log(2, 2);
+else
+if(a > 15)
+console.log(3, 3);
+else
+console.log(4, 4);
+else
+console.log(5, 5);
+//Atividade 2
+let a = 5;
+let b = ++a/2;
+console.log(b--);
+
+//Atividade3
+let livro = {
+    titulo: "JavaScript: o guia definitivo",
+    edicao: 6,
+    autor: "David Flanagar",
+    editora: "O 'Reilly'",
+    ano: 2012
+};
+
+console.log(1, livro.titulo);
+
+console.log(2, livro['edicao']);
+
+let prop = 'editora';
+console.log(3, livro[prop]);
+
+*/
+
+
