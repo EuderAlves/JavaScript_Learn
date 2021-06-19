b3e140a7811d918b941c4192aa222610cae4542c
Euder Alves <euder.alv@gmail.com>
2021-06-19 15:56:55 -0300

    Obejto

---
+++ JavaScript/Objetos.js
@@ -0,0 +1,23 @@
+let musica = {};
+console.log(musica);
+
+musica.nome = "I'm eighteen";
+musica.artista = "Alice Cooper";
+musica.segundos = 180;
+
+console.log(1, musica);
+
+//função apra cirar objetos
+
+function criaUsuario(nome, idade) {
+    return{
+        nome: nome,
+        idade: idade,
+    };
+}
+
+let joao = criaUsuario (`João`, 35);
+
+let maria = criaUsuario("Maria", 24);
+
+console.log(2, joao, 3, maria);
