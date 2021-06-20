fd134fd6ca095512a7d52ca892b3fa6cf0095e4d
Euder Alves <euder.alv@gmail.com>
2021-06-20 10:42:15 -0300

    OnmouseOver anda OnmouseOut

---
+++ JavaScript/Script/Enventos.js
@@ -0,0 +1,51 @@
+/*
+---Principais Eventos---
+
+*click - quando o mouse é clicado em um 
+elemento da página;
+
+*mouseover - quando o mouse entre sobre 
+a área de um elemento;
+
+*mouseout - quando o mouse deixa a área
+de um elemento;
+
+*mousemove - quando o mouse  é movimentado;
+
+*contextmenu - quando o botão direito do 
+mouse é clicado;
+
+*mousedown - quando o botão do mouse
+é pressionado;
+
+*mouseup - quando o botão do mouse é solto;
+
+---Eventos de Formulario---
+
+ coloca "on" antes de cada evento abaixo.
+
+*submit - quando um formulário é enviado;
+
+*focus - quando um campo do formulário é ativado;
+
+*blur - quando o usuário deixa um campo do formulário;
+
+*change - quando o valor de um campo é alterado;
+
+*select - quando é feita uma escolha em uma caixa de seleção;
+
+---Evento de Teclado---
+
+*keydown - quando uma tecla é pressionada;
+
+*keyup - quando a tecla é liberada;
+
+---Evento de Janela---
+
+*load - quando a página termina de ser carregada;
+
+*unload - quando o usuário deixa ou fecha o navegador;
+
+*rezise - quando a janela do navegador é redimensionada;
+
+*/
